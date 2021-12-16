14bf20f5f4ddfe57382467058c11b2e8ca79ec55
alex law <alexlawcodes@gmail.com>
2021-12-16 14:08:01 +0000

    covid data updated

--- data_clean.py
+++ data_clean.py
@@ -1,16 +1,17 @@
 import pandas as pd
+from tqdm import tqdm
 
 def formatdf(df):
-    #Drop unnecessary columns and rows.
-    to_drop = ['Province/State','Lat','Long']
-    df.drop(labels=to_drop, axis=1, inplace=True)
-    df.drop(0, axis=0, inplace=True)
+    #Drop unnecessary columns
+    df = df.drop(labels=['Lat', 'Long', 'Province/State'], axis=1)
+    #Melt coluns ready for further processing
+    df_m = df.melt(id_vars=['Country/Region'], var_name='Date', value_name='Value')
     #Convert string values to ints.
-    df['Value'] = pd.to_numeric(df['Value'])
+    df_m['Value'] = pd.to_numeric(df_m['Value'])
     #Sum entries with same date and country (but different region).
-    df = df.groupby(['Country/Region', 'Date']).sum()
-    df.reset_index(inplace=True)
-    return df
+    df_m = df_m.groupby(['Country/Region', 'Date']).sum()
+    df_m.reset_index(inplace=True)
+    return df_m
 
 def getCountries(cases_df):
     #Get list of countries for dropdown
@@ -19,7 +20,7 @@ def getCountries(cases_df):
 def getPerDayWeek(df, countries):
     #Get per day deaths from difference in cumulative for each country.
     per_day_week_df = pd.DataFrame(columns=['Country/Region','Date','Value','Type','Diff','Week'])
-    for country in countries:
+    for country in tqdm(countries):
         country_df = df[df['Country/Region']==country]
         day_series = df[df['Country/Region']==country]['Value'].diff()
         week_series = df[df['Country/Region']==country]['Value'].diff(7)
@@ -29,9 +30,10 @@ def getPerDayWeek(df, countries):
         per_day_week_df = per_day_week_df.append(temp_df)
     return per_day_week_df
 
-#Get data frames
-deaths_df = pd.read_csv('covid_deaths.csv')
-cases_df = pd.read_csv('covid_cases.csv')
+#Get data frames   
+cases_df = pd.read_csv(r'C:/Users/alexw/coding-projects/covid-dash/data_repo/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+deaths_df = pd.read_csv(r'C:/Users/alexw/coding-projects/covid-dash/data_repo/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+
 
 #Format data frames.
 deaths_df = formatdf(deaths_df)
